3feab2bccf62b7222962bbf039cdb0914cc18d01
JeppeMldrup <36853067+JeppeMldrup@users.noreply.github.com>
2018-07-14 11:04:46 +0200

    My Planck Layout

--- keyboards/planck/keymaps/ALayout/keymap.c
+++ keyboards/planck/keymaps/ALayout/keymap.c
@@ -18,14 +18,14 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = {
-	{KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL },
+	{KC_GESC, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL },
 	{KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
 	{KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
 	{KC_LCTL, MO(_FN), KC_LALT, KC_LGUI,MO(_LEFT),KC_LSFT, KC_SPC,MO(_RIGHT),KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
 [_QWERTY] = {
-	{KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL },
+	{KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL },
 	{KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
 	{KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
 	{KC_LCTL, MO(_FN), KC_LALT, KC_LGUI,MO(_LEFT),KC_LSFT, KC_SPC,MO(_RIGHT),KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	},
 
 [_LEFT] = {
-	{KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_TRNS},
+	{KC_TILD, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_TRNS},
 	{KC_BSPC, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_TRNS, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_TRNS},
-	{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_TRNS},
+	{KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_UNDS, KC_TRNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_TRNS},
 	{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 
